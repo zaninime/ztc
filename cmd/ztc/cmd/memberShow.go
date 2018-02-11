c63edf63006146de5eaafb3a2724335b31fde037
Francesco Zanini <francesco@zanini.me>
2018-02-11 21:06:43 +0100

    Remove all the bullshit

--- cmd/ztc/cmd/memberShow.go
+++ cmd/ztc/cmd/memberShow.go
@@ -1,17 +1,3 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -20,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// memberShowCmd represents the memberShow command
 var memberShowCmd = &cobra.Command{
 	Use:   "show [node-id]",
 	Short: "List members or show details about one",
@@ -32,14 +17,4 @@ var memberShowCmd = &cobra.Command{
 
 func init() {
 	memberCmd.AddCommand(memberShowCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// memberShowCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// memberShowCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
